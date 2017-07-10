d416899bab8fad17cf4f716901ab59cc38ac4854
kobeli <lsy1217@163.com>
2017-07-10 21:52:53 +0800

    first commit

---
+++ MobileTerminaliOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MobileTerminaliOS
+//
+//  Created by kobeli on 2017/7/10.
+//  Copyright © 2017年 robotlinker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MobileTerminaliOS/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MobileTerminaliOS
+//
+//  Created by kobeli on 2017/7/10.
+//  Copyright © 2017年 robotlinker. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
